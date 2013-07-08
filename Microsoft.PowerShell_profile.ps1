f6eadee17f93940188c2cedc82247077f7be65dc
Steve Hocking <github@stevehocking.co.uk>
2013-07-08 15:29:19 +0100

    Initial commit of my current profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,50 @@
+﻿. C:\Users\steven.hocking\AppData\Local\GitHub\shell.ps1
+Import-Module Posh-Git
+Import-Module Posh-Hg
+
+function isCurrentDirectoryARepository($type) {
+
+    if ((Test-Path $type) -eq $TRUE) {
+        return $TRUE
+    }
+
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/' + $type;
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    return $FALSE
+}
+
+# Posh-Hg and Posh-git prompt
+
+#. $ProfileRoot\Modules\posh-hg\profile.example.ps1
+#. $ProfileRoot\Modules\posh-git\profile.example.ps1
+
+function prompt(){
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host $pwd -nonewline
+
+    if (isCurrentDirectoryARepository(".git")) {
+        # Git Prompt
+        $Global:GitStatus = Get-GitStatus
+        Write-GitStatus $GitStatus
+    } elseif (isCurrentDirectoryARepository(".hg")) {
+        # Mercurial Prompt
+        $Global:HgStatus = Get-HgStatus
+        Write-HgStatus $HgStatus
+    }
+
+    return "> "
+}
+
+Enable-GitColors
+
+Start-SshAgent -Quiet
---
+++ README.md
@@ -0,0 +1,15 @@
+Utilities
+=========
+
+Getting started
+---------------
+
+1. Clone the powershell-profile to your local machine
+
+2. [Correctly install the *Utilities* module](http://msdn.microsoft.com/en-us/library/windows/desktop/dd878350.aspx)
+
+3. [Open your profile for editing](http://blogs.technet.com/b/heyscriptingguy/archive/2012/05/21/understanding-the-six-powershell-profiles.aspx)
+
+4. Add the following into your profile:	`Import-Module Utilities`
+	
+5. Restart powershell!
