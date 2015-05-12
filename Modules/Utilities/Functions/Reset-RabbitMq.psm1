c042db288621bd651f3db5adf95d95c810e075e0
Steve Hocking <steve@codemunki.es>
2015-05-12 09:35:00 +0100

    Added Reset-RabbitMq and Start-HlJobScheduler cmdlets

---
+++ Modules/Utilities/Functions/Reset-RabbitMq.psm1
@@ -0,0 +1,8 @@
+﻿Function Reset-RabbitMq {
+  $path = "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.0\sbin\rabbitmqctl.bat"
+  $arguments = @("stop_app", "reset", "start_app")
+
+  for ($i = 0; $i -lt $arguments.length; $i++) {
+    Start-Process $path $arguments[$i] -NoNewWindow -Wait
+  }
+}
---
+++ Modules/Utilities/Functions/Start-HlJobScheduler.psm1
@@ -0,0 +1,19 @@
+﻿Function Start-HlJobScheduler {
+    param([string]$platform = "ot-uk")
+
+    $path = $null
+
+    if($platform.ToLower() -eq "hh-us") {
+        $path = "D:\git\cprm\src\core\Havas.CareForToday.Consumers.Host.HeartHealth.US\bin\Debug\Havas.CareForToday.Consumers.Host.HeartHealth.US.exe"
+    } elseif($platform.ToLower() -eq "hh-uk") {
+        $path = "D:\git\cprm\src\core\Havas.CareForToday.Consumers.Host.HeartHealth.UK\bin\Debug\Havas.CareForToday.Consumers.Host.HeartHealth.UK.exe"
+    } elseif($platform.ToLower() -eq "ot-uk") {
+        $path = "D:\git\cprm\src\core\Havas.CareForToday.JobScheduler.Host.OrthopaedicTherapy.UK\bin\Debug\Havas.CareForToday.JobScheduler.Host.OrthopaedicTherapy.UK.exe"
+    }
+
+    if($path -eq $null) {
+        return
+    }
+
+    Start-Process $path
+}
